70cfb0c702becf958650544d1b9c3a1d05d60017
YvanWa <yvanwang26@crosspointacademy.org>
2025-09-13 06:00:09 +0000

    calculator

---
+++ Calculator.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Make your arithmetic selection from the choices below:\n");
+        System.out.println("  1. Addition");
+        System.out.println("  2. Subtraction");
+        System.out.println("  3. Multiplication");
+        System.out.println("  4. Division\n");
+
+        System.out.print("Your choice? ");
+        Scanner kbReader = new Scanner(System.in);
+        int choice = kbReader.nextInt();
+
+        System.out.print("\nEnter first operand: ");
+        double op1 = kbReader.nextDouble();
+
+        System.out.print("Enter second operand: ");
+        double op2 = kbReader.nextDouble();
+
+        System.out.println("");
+
+        switch (choice) {
+            case 1: // addition
+                System.out.println(op1 + " plus " + op2 + " = " + (op1 + op2));
+                break;
+            case 2: // subtraction
+                System.out.println(op1 + " minus " + op2 + " = " + (op1 - op2));
+                break;
+            case 3: // multiplication
+                System.out.println(op1 + " times " + op2 + " = " + (op1 * op2));
+                break;
+            case 4: // division
+                System.out.println(op1 + " divided by " + op2 + " = " + (op1 / op2));
+                break;
+            default:
+                System.out.println("Hey dummy, enter only a 1, 2, 3, or 4!");
+        }
+
+        kbReader.close();
+    }
+}
